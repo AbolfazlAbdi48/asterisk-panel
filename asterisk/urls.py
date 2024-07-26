0326187340013cde40fe264e1a8fc5534a180907
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2024-07-26 15:17:54 +0330

    add: get logs view, ajax

--- asterisk/urls.py
+++ asterisk/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, get_logs_view
 
 app_name = 'asterisk'
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('get_logs/', get_logs_view, name='get_logs'),
 ]
--- asterisk/views.py
+++ asterisk/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'asterisk/home.html')
+
+
+def get_logs_view(request):
+    return render(request, 'asterisk/get_logs.html')
